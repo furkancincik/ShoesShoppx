2b888145a51b06f9071bf3cc0840d86af4068b38
nonickpick <148152625+nonickpick@users.noreply.github.com>
2024-01-03 11:33:11 +0300

    güncel

--- src/Shoes.java
+++ src/Shoes.java
@@ -16,6 +16,13 @@ public class Shoes {
     public static Shoes[] shoes(){
         Shoes[] shoeList = new Shoes[100];
         shoeList[0] = new Shoes(1,"Sneaker","Nike Dunk Low",43,3000);
+        shoeList[1] = new Shoes(2,"Sneaker","Nike AirForce1",42,3500);
+        shoeList[2] = new Shoes(3,"Sneaker","Nike Huarache",39,2999);
+        shoeList[3] = new Shoes(4,"Sneaker","Adidas Ozweego",44,2100);
+        shoeList[4] = new Shoes(5,"Sneaker","Adidas Nite Jogger",41,1900);
+        shoeList[5] = new Shoes(6,"Sneaker","Puma -180",41,1800);
+        shoeList[6] = new Shoes(7,"Sneaker","Adidas Superstar",41,2400);
+        shoeList[7] = new Shoes(8,"Sneaker","Adidas Stan Smith",38,2600);
         return shoeList;
     }
 
